0eb923919ae473d560d8b7e0d76597dda6d57e89
OlegKrivenko <krivenko.olegus@gmail.com>
2023-07-26 16:00:30 +0300

    comments removed and changed App

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,24 @@ import ContactEditor from './components/ContactEditor';
 import ContactList from './components/ContactList';
 import ContactFilter from './components/ContactFilter';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contactsLocalStorage = localStorage.getItem('contacts');
-    const paresdContacts = JSON.parse(contactsLocalStorage);
-
-    return paresdContacts.length !== 0
-      ? paresdContacts
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ];
-  });
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? initialContacts
+  );
   const [filter, setFilter] = useState('');
-
-  // componentDidMount() {
-  //   // console.log('App componentDidMount');
-
-  //   const contacts = localStorage.getItem('contacts');
-  //   const paresdContacts = JSON.parse(contacts);
-
-  //   if (paresdContacts) {
-  //     this.setState({ contacts: paresdContacts });
-  //   }
-  // }
 
   useEffect(() => {
     console.log('Оновилось поле contacts, записую contacts в локальне сховище');
 
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   // console.log('App componentDidUpdate');
-
-  //   const nextContacts = this.state.contacts;
-  //   const prevContacts = prevState.contacts;
-
-  //   if (prevContacts !== nextContacts) {
-  //     console.log(
-  //       'Оновилось поле contacts, записую contacts в локальне сховище'
-  //     );
-
-  //     localStorage.setItem('contacts', JSON.stringify(nextContacts));
-  //   }
-  // }
 
   const addContact = ({ name, number }) => {
     const isExist = contacts.find(
@@ -70,39 +41,13 @@ const App = () => {
     setContacts(prev => [...prev, contact]);
   };
 
-  // addContact = ({ name, number }) => {
-  //   const isExist = this.state.contacts.find(
-  //     contact => contact.name.toLowerCase() === name.toLowerCase()
-  //   );
-
-  //   if (isExist) {
-  //     Notify.failure(`${name} is already in contacts`);
-  //     return;
-  //   }
-
-  //   const contact = { id: nanoid(), name, number };
-  //   this.setState(({ contacts }) => ({
-  //     contacts: [contact, ...contacts],
-  //   }));
-  // };
-
   const deleteContact = conId => {
     setContacts(prev => prev.filter(contact => contact.id !== conId));
   };
 
-  // deleteContact = conId => {
-  //   this.setState(prevState => ({
-  //     contacts: prevState.contacts.filter(contact => contact.id !== conId),
-  //   }));
-  // };
-
   const changeFilter = event => {
     setFilter(event.currentTarget.value);
   };
-
-  // changeFilter = event => {
-  //   this.setState({ filter: event.currentTarget.value });
-  // };
 
   const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
@@ -111,15 +56,6 @@ const App = () => {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-
-  // getVisibleContacts = () => {
-  //   const { contacts, filter } = this.state;
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
 
   const visibleContacts = getVisibleContacts();
 
@@ -140,101 +76,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     // console.log('App componentDidMount');
-
-//     const contacts = localStorage.getItem('contacts');
-//     const paresdContacts = JSON.parse(contacts);
-
-//     if (paresdContacts) {
-//       this.setState({ contacts: paresdContacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     // console.log('App componentDidUpdate');
-
-//     const nextContacts = this.state.contacts;
-//     const prevContacts = prevState.contacts;
-
-//     if (prevContacts !== nextContacts) {
-//       console.log(
-//         'Оновилось поле contacts, записую contacts в локальне сховище'
-//       );
-
-//       localStorage.setItem('contacts', JSON.stringify(nextContacts));
-//     }
-//   }
-
-//   addContact = ({ name, number }) => {
-//     const isExist = this.state.contacts.find(
-//       contact => contact.name.toLowerCase() === name.toLowerCase()
-//     );
-
-//     if (isExist) {
-//       Notify.failure(`${name} is already in contacts`);
-//       return;
-//     }
-
-//     const contact = { id: nanoid(), name, number };
-//     this.setState(({ contacts }) => ({
-//       contacts: [contact, ...contacts],
-//     }));
-//   };
-
-//   deleteContact = conId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== conId),
-//     }));
-//   };
-
-//   changeFilter = event => {
-//     this.setState({ filter: event.currentTarget.value });
-//   };
-
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   render() {
-//     console.log('App render');
-
-//     const { filter } = this.state;
-//     const visibleContacts = this.getVisibleContacts();
-
-//     return (
-//       <Container>
-//         <h1>Phonebook</h1>
-//         <ContactEditor onSubmit={this.addContact} />
-
-//         <h2>Contacts</h2>
-//         <ContactFilter
-//           value={filter}
-//           onChange={this.changeFilter}
-//         ></ContactFilter>
-
-//         <ContactList
-//           contacts={visibleContacts}
-//           onDeleteContact={this.deleteContact}
-//         ></ContactList>
-//       </Container>
-//     );
-//   }
-// }
--- src/components/ContactEditor/ContactEditor.jsx
+++ src/components/ContactEditor/ContactEditor.jsx
@@ -5,7 +5,6 @@ import css from './ContactEditor.module.css';
 const ContactEditor = ({ onSubmit }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-  // state = { name: '', number: '' };
 
   const handleChange = event => {
     const { name: nameNew, value } = event.currentTarget;
@@ -22,24 +21,12 @@ const ContactEditor = ({ onSubmit }) => {
       default:
     }
   };
-  // handleChange = event => {
-  //   const { name, value } = event.currentTarget;
-  //   this.setState({ [name]: value });
-  // };
 
   const handleSubmit = event => {
     event.preventDefault();
     onSubmit({ name, number });
     resetForm(event);
   };
-  // handleSubmit = event => {
-  //   console.log(event);
-  //   event.preventDefault();
-
-  //   this.props.onSubmit(this.state);
-
-  //   this.resetForm(event);
-  // };
 
   const resetForm = event => {
     setName('');
@@ -48,12 +35,6 @@ const ContactEditor = ({ onSubmit }) => {
     event.target.elements.name.value = '';
     event.target.elements.number.value = '';
   };
-  // resetForm = event => {
-  //   this.setState({ name: '', number: '' });
-
-  //   event.target.elements.name.value = '';
-  //   event.target.elements.number.value = '';
-  // };
 
   return (
     <form className={css.form} onSubmit={handleSubmit}>
@@ -91,64 +72,3 @@ const ContactEditor = ({ onSubmit }) => {
 };
 
 export default ContactEditor;
-
-// class ContactEditor extends Component {
-//   state = { name: '', number: '' };
-
-//   handleChange = event => {
-//     const { name, value } = event.currentTarget;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = event => {
-//     console.log(event);
-//     event.preventDefault();
-
-//     this.props.onSubmit(this.state);
-
-//     this.resetForm(event);
-//   };
-
-//   resetForm = event => {
-//     this.setState({ name: '', number: '' });
-
-//     event.target.elements.name.value = '';
-//     event.target.elements.number.value = '';
-//   };
-
-//   render() {
-//     return (
-//       <form className={css.form} onSubmit={this.handleSubmit}>
-//         <label className={css.label}>
-//           Name
-//           <input
-//             className={css.input}
-//             type="text"
-//             name="name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//             onChange={this.handleChange}
-//           />
-//         </label>
-
-//         <label className={css.label}>
-//           Number
-//           <input
-//             className={css.input}
-//             type="tel"
-//             name="number"
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//             onChange={this.handleChange}
-//           />
-//         </label>
-
-//         <button type="submit" className={css.button}>
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
